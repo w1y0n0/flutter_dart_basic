23a64466f66166f4e86f32c6446c2e6e2dfe2610
w1y0n0 <wiyono30@gmail.com>
2023-08-01 13:27:44 +0700

    data type & variable

---
+++ demo.dart
@@ -0,0 +1,20 @@
+// nodemon --exec dart demo.dart
+
+class Product {
+  static String productName = "Digital Product";
+}
+
+void main() {
+  // print("Hello World!!!");
+
+  // deklarasi varabel
+  // tipe_data nama_variabel = initial_value;
+  String name = "LabSI Project";
+  int price = 250;
+  double discount = 25.23;
+  bool isLogin = false; //true|false
+
+  print("Hello, nama saya " + name);
+  print("Hello, nama saya $name");
+  print("Hello, nama saya ${Product.productName}");
+}
