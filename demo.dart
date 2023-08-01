e50e9bef84a31ecd2daa46d66518d972d737846b
w1y0n0 <wiyono30@gmail.com>
2023-08-01 13:56:41 +0700

    Null Safety

--- demo.dart
+++ demo.dart
@@ -14,7 +14,14 @@ void main() {
   double discount = 25.23;
   bool isLogin = false; //true|false
 
+  // Null Safety
+  double? harga = 25000;
+  double? qty; //nullable
+  double total = harga * (qty ?? 0);
+
+  // Output
   print("Hello, nama saya " + name);
   print("Hello, nama saya $name");
   print("Hello, nama saya ${Product.productName}");
+  print(total);
 }
