3e8347797eba7ce9d7773447a1a691873c155fc6
w1y0n0 <wiyono30@gmail.com>
2023-08-01 09:54:22 +0700

    update list items

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,56 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    // buat list items
+    final List<String> items = [
+      'Data types & variable',
+      'Null Safety',
+      'Operator',
+      'IF Statement',
+      'Map',
+      'List',
+      'Looping',
+      'Functions',
+      'Anonymous Functions',
+      'Enum',
+    ];
+    int no = 0;
+
+    return MaterialApp(
+      title: "Flutter: Dart Basic",
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text('Dart Basic'),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            const SizedBox(height: 16),
+            Expanded(
+              child: ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  no += 1;
+                  return Padding(
+                    padding: const EdgeInsets.fromLTRB(16, 2, 16, 2),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.blueGrey.shade300,
+                        child: Text(
+                          no.toString(),
+                          style: const TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      title: Text(items[index]),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
